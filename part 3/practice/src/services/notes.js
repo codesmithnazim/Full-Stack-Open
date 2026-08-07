09847b52d1be9da9c95b5c15690bde640468b74d
codesmithnazim <codesmithnazim@gmail.com>
2026-08-07 20:41:35 +0500

    Express_Domain updated

--- part 3/practice/src/services/notes.js
+++ part 3/practice/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://fullstackopen-e790a.containers.snapdeploy.app/api/notes'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
