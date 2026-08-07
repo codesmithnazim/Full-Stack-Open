6c79a2ce5c326dde11691ea4c927695e12922afa
codesmithnazim <codesmithnazim@gmail.com>
2026-08-07 20:03:42 +0500

    Isnha'Allah I'll  deploy my first express server through my hands

--- part 3/practice/axios.js
+++
@@ -1,3 +0,0 @@
-import axios from "axios";
-
-axios.get("http://localhost:3001/notes").then(res=> console.log(res.status,res.ok, res.data))
---
+++ part 3/practice/express.js
@@ -0,0 +1,56 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import path from "path";
+const app = express();
+const PORT = process.env.EXPRESS_PORT;
+
+app.use(express.json());
+app.use(express.static(path.join(process.cwd(),"dist")));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  }),
+);
+
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app
+  .get("/api/notes", (req, res) => {
+    res.send(notes);
+  })
+  .post("/api/notes", (req, res) => {
+    console.log("req body", req.body);
+    if (!req.body.content) {
+      console.log("the content part of the note is empty");
+      res.status(400).send({ error: "Every note should have content" });
+      return;
+    }
+    let maxId = Math.max(...notes.map((each) => each.id));
+    let newNote = req.body;
+    newNote.id = maxId + 1;
+    notes.push(newNote);
+    res.send(newNote);
+    console.log("the new Note = ", newNote);
+  });
+
+app.listen(PORT, () => {
+  console.log("Express server is runing at port = ", PORT);
+});
